25d87a94b6b5d8790f86d702086899bacb2c1a04
jiemei0754 <66130861+jiemei0754@users.noreply.github.com>
2021-08-08 22:50:16 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -4,20 +4,20 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 # 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
-git clone https://github.com/jianping0754/luci-theme-darkmatter.git package/lean/luci-theme-darkmatter  #暗黑主题
-git clone https://github.com/jianping0754/luci-app-jd-dailybonus  #京东签到插件
+git clone https://github.com/jiemei0754/luci-theme-darkmatter.git package/lean/luci-theme-darkmatter  #暗黑主题
+git clone https://github.com/jiemei0754/luci-app-jd-dailybonus  #京东签到插件
 
 cat >$NETIP <<-EOF
-uci set network.lan.ipaddr='192.168.31.3'                                   # IPv4 地址(openwrt后台地址)
+uci set network.lan.ipaddr='192.168.32.4'                                   # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-uci set network.lan.gateway='192.168.31.1'                                  # IPv4 网关
-uci set network.lan.broadcast='192.168.31.255'                              # IPv4 广播
-uci set network.lan.dns='192.168.31.201 192.168.31.1'                       # DNS(多个DNS要用空格分开)
+uci set network.lan.gateway='192.168.32.2'                                  # IPv4 网关
+uci set network.lan.broadcast='192.168.32.255'                              # IPv4 广播
+uci set network.lan.dns='192.168.32.202 192.168.32.2'                       # DNS(多个DNS要用空格分开)
 uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
-#uci set dhcp.lan.ignore='1'                                                # 关闭DHCP功能
-#uci commit dhcp                                                            # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
-uci set system.@system[0].hostname='OpenWrt-GT'                             # 修改主机名称为OpenWrt-GT
+uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
+uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
+uci set system.@system[0].hostname='OpenWrt-RT'                             # 修改主机名称为OpenWrt-RT
 EOF
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile            # 选择argon为默认主题
